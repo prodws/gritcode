82f212fb6f80f0649dae62ca6d611998fe53595d
prodws <169688005+prodws@users.noreply.github.com>
2026-05-29 15:20:19 +0200

    feat: add global GraphQL error handler

---
+++ backend/src/main/java/com/github/prodws/codingplatform/config/GraphQlExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.github.prodws.codingplatform.config;
+
+import graphql.GraphQLError;
+import graphql.schema.DataFetchingEnvironment;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphQlExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        if (ex instanceof ConstraintViolationException cve) {
+            String message = cve.getConstraintViolations()
+                    .iterator().next()
+                    .getMessage();
+            return GraphQLError.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(message)
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        }
+        if (ex instanceof IllegalStateException) {
+            return GraphQLError.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(ex.getMessage())
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        }
+        return null;
+    }
+}
